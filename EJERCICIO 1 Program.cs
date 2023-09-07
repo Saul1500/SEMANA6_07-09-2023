493f59a725d31a2adc496c1f6e356b354ff85f85
Saul1500 <125324281+Saul1500@users.noreply.github.com>
2023-09-06 19:50:31 -0600

    Add files via upload

---
+++ EJERCICIO 1 Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = { 97, 92, 81, 60 };
+
+        var queryLowNums =
+            from score in scores
+            where score > 80
+            select score;
+
+        foreach (var i in queryLowNums)
+        {
+            Console.WriteLine(i.ToString() + " ");
+        }
+    }
+}
+//ALUMNO: JOSE SAUL SIBRIAN SERRANO
+
+
---
+++ EJERCICIO 2 Program.cs
@@ -0,0 +1,25 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+class Program
+{
+    static void Main()
+    {
+        int[] scores = { 90, 71, 82, 93, 75, 82 };
+
+        var queryLowNums =
+            from score in scores
+            where score > 80
+            orderby score descending
+            select score;
+
+
+
+        foreach (var s in queryLowNums)
+        {
+            Console.WriteLine(s.ToString() + " ");
+        }
+
+    }
+}
+//ALUMNO: JOSE SAUL SIBRIAN SERRANO
